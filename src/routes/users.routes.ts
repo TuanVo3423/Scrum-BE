f765d7c83e34dfdb7d47cec603d107bc5420b23e
Vo Van Tuan <tuanvv.21it@vku.udn.vn>
2025-05-14 10:28:51 +0700

    feature: forgot password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -45,7 +45,7 @@ usersRouter.post('/register', registerValidator, wrapRequestHandler(registerCont
 usersRouter.post('/mail-verify-token', accessTokenValidator, wrapRequestHandler(MailVerifyTokenController))
 usersRouter.post('/verify-email', emailVerifyTokenValidator, wrapRequestHandler(verifyEmailController))
 usersRouter.post('/resend-verify-email', accessTokenValidator, wrapRequestHandler(resendVerifyEmailController))
-usersRouter.post('/forgot-password', forgotPasswordValidator, wrapRequestHandler(forgotPasswordController))
+  usersRouter.post('/forgot-password', forgotPasswordValidator, wrapRequestHandler(forgotPasswordController))
 usersRouter.post(
   '/verify-forgot-password',
   verifyForgotPasswordTokenValidator,
